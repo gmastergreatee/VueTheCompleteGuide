777afbd97d2da6a547501f94ed60c9d2eeaaff2d
gmastergreatee <g77471164@gmail.com>
2020-11-15 12:01:10 +0530

    01_1

--- 01-AFirstApp/app.js
+++ 01-AFirstApp/app.js
@@ -1,4 +1,17 @@
-
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: '',
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        },
+    },
+}).mount('#app');
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
